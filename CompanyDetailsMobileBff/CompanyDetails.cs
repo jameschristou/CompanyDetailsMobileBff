0ea8f839c4577857514bf4cfedce4b76f9298dc1
James Christou <james.christou@gmail.com>
2021-02-19 15:28:06 +1100

    Initial commit

---
+++ CompanyDetailsMobileBff/CompanyDetails.cs
@@ -0,0 +1,8 @@
+﻿namespace CompanyDetailsMobileBff
+{
+    public class CompanyDetails
+    {
+        public string Name { get; set; }
+        public string AddressFormatted { get; set; }
+    }
+}
---
+++ CompanyDetailsMobileBff/Controllers/CompanyDetailsController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyDetailsMobileBff.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CompanyDetailsController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<CompanyDetailsController> _logger;
+
+        public CompanyDetailsController(ILogger<CompanyDetailsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<CompanyDetails> Get()
+        {
+            return new List<CompanyDetails>
+            {
+                new CompanyDetails
+                {
+                    Name = "Google",
+                    AddressFormatted = ""
+                },
+                new CompanyDetails
+                {
+                    Name = "Slack",
+                    AddressFormatted = ""
+                }
+            };
+        }
+    }
+}
